35d7a1ca38694e01eb18cd3f39bb9445edf717e0
Manuel Gil Cuena <mgcuena@gmail.com>
2022-09-18 14:20:01 +0200

    First basic test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import { useState, useReducer } from "react";
 
-function App() {
+const test = [
+  {id: 0, question: "question1", option1: "option1", option2: "option2", answer: "option1" },
+  {id: 1, question: "question2", option1: "option1", option2: "option2", answer: "option2" },
+  {id: 2, question: "question3", option1: "option1", option2: "option2", answer: "option2" }
+];
+
+function App() {  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header description="App for training"/>
+      <Main />
+      <Questions 
+        data={test}
+        renderEmpty={<p>No questions</p>}
+        renderItem={(item) => (
+        <ul>
+          <p>{item.question}</p>
+            <li>
+            <input type="checkbox" />
+            <label>{item.option1}</label>
+            </li>
+            <li>
+            <input type="checkbox" />
+            <label>{item.option2}</label>
+            </li>
+        </ul>
+        )}
+      />
     </div>
+  );
+}
+
+function Header({description}) {
+  return (       
+    <header>
+      <h1>{description}</h1>
+    </header>
+  );
+}
+
+function Main() {
+  const [status, setStatus] = useState("Are you ready?");
+
+  return (
+    <div className="App">
+      <p>{status}</p>
+      <button onClick={() => setStatus("Let's start!")}>Start</button>
+    </div>
+  );
+}
+
+function Questions({ data, renderItem, renderEmpty }) {
+  return !data.length ? ( renderEmpty ) :
+  (
+    <ul>
+      {data.map((item) => (
+        <li key={item.id}>
+          {renderItem(item)}
+        </li>
+      ))}
+    </ul>
   );
 }
 
