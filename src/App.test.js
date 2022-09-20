462a93f0c34211a8aa90220d89f44d14e54286cc
Manuel Gil Cuena <mgcuena@gmail.com>
2022-09-20 13:26:39 +0200

    Added first test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import { MemoryRouter } from 'react-router-dom'
+import { App } from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('Header component should be rendered', () => {
+  render(<MemoryRouter><App /></MemoryRouter>);
+  const headerElement = screen.getByText(/App for training/i);
+  expect(headerElement).toBeInTheDocument();
 });
